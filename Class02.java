a8cc92cdeb4eca45fc8377d86fbc04cf17a9cacc
110b26227hana <114041580+110b26227hana@users.noreply.github.com>
2022-11-08 14:06:57 +0800

    Create Class02.java

---
+++ Class02.java
@@ -0,0 +1,21 @@
+absrtact class CSahpe
+{
+  protected ST5ring color;
+  public void setColor(Stribg str)
+  {
+    color=str;
+  }
+  public abstract void nshow();
+}
+class CRectangle extends CSape
+{
+ protected int width, height;
+ publeic CRectangle(int w, int h)
+ {
+  width = w;
+  height = h;
+  }
+  publeic void show()
+  {
+   System.out.print("colcor="+color+");
+   System.out.println("area="+width*height);
