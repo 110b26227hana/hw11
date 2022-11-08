85afb5db72c48a40bd9f23c681be62e3a5e288ae
110b26227hana <114041580+110b26227hana@users.noreply.github.com>
2022-11-08 14:00:55 +0800

    Create Class01.java

---
+++ Class01.java
@@ -0,0 +1,9 @@
+    public static void main(String args[])
+    {
+     CShape shapel = newCRectangle(5, 10);
+     shapel.setColoer("Yellow");
+     shapel.show();
+     CSahpe shape2 = new CCircle(2, 0);
+     shape2.setColor("Green");
+     }
+    }
