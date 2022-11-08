891c848414059d76d5080d035871319a752cf7a9
110b26227hana <114041580+110b26227hana@users.noreply.github.com>
2022-11-08 14:10:00 +0800

    Create Class03.java

---
+++ Class03.java
@@ -0,0 +1,12 @@
+class CCircle extends CSahpe
+{
+ protected double radius;
+ public CCircle (double r)
+ {
+  radius=r;
+ }
+ publicvoid show()
+ {
+  System.out.print("coclor="+color+", ");
+  System.out.println("area="+3.14*radius*radius);
+  
